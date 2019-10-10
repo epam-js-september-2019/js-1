fff2ae87de79078904069c7cbc67d12345184e7d
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2019-10-11 01:52:55 +0300

    Task 3 completed

--- homework.js
+++ homework.js
@@ -1,7 +1,12 @@
+
+/*      TASK 1 SPLIT AND MERGE      */
 const splitAndMerge = (str, delim) => {
     return str.replace(' ','').split('').join(delim);
-}
+};
+
 console.log(splitAndMerge('Hello world', ','));
+
+/*      TASK 2 HASH TO ARR      */
 
 const hashToArr = (obj) => {
     let result = [];
@@ -11,5 +16,17 @@ const hashToArr = (obj) => {
         result.push(arr);
     }
     return result;
-}
+};
+
 console.log(hashToArr({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
+
+/*      TASK 3 SPLIT AND MERGE      */
+
+const toCamelCasing = (str) => {
+    let a = str.split(str[str.search(/(_|-)/)]);
+    return a.map(item => item[0].toLocaleUpperCase() + item.substr(1)).join('');
+};
+
+console.log(toCamelCasing('Test_gdf_sas'));
+
+/*      TASK 4 SPLIT AND MERGE      */
