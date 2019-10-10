babd6141c5e4da94d509b322fb9b64518a99c264
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2019-10-11 00:37:51 +0300

    Task 2 completed

--- homework.js
+++ homework.js
@@ -2,3 +2,14 @@ const splitAndMerge = (str, delim) => {
     return str.replace(' ','').split('').join(delim);
 }
 console.log(splitAndMerge('Hello world', ','));
+
+const hashToArr = (obj) => {
+    let result = [];
+    let arr = [];
+    for (const item in obj) {
+        arr = [item, obj[item]];
+        result.push(arr);
+    }
+    return result;
+}
+console.log(hashToArr({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
